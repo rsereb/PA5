f188708535af869c0a9082be4c5fec4ab0f30c5c
rsereb <rvsereb93@gmail.com>
2022-10-27 01:59:11 -0700

    initial setup

---
+++ functions.cpp
@@ -0,0 +1,5 @@
+#include "functions.h"
+void ruslan()
+{
+    cout << "hello my name is ruslan";
+}
---
+++ functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <iostream>
+
+void ruslan();
+
+
+
+
+
+#endif
